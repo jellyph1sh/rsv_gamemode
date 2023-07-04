fa0651a84673dd1011d7feda5d3af2154331b9b0
JellyPh1sh <sacha.sorgiati@gmail.com>
2023-07-04 17:10:02 +0200

    :heavy_plus_sign: | Add exports from rsv_utils.

--- server/sv_gamemode.lua
+++ server/sv_gamemode.lua
@@ -1,13 +1,6 @@
-function SendChatMessage(src, messages, colour)
-    TriggerClientEvent("chat:addMessage", src, {
-        args = messages,
-        color = colour
-    })
-end
-
 RegisterCommand("serverid", function(src)
     if (src > 0) then
-        SendChatMessage(src, {"ID:", src}, { 255, 255, 255 })
+        exports.rsv_utils:SendChatMessage(src, {"ID:", src}, { 255, 255, 255 })
     else
         print("This command can be used by players only!")
     end
@@ -16,16 +9,16 @@ end, false)
 RegisterCommand("revive", function(src, args)
     if (#args == 0) then
         TriggerClientEvent("jph_gamemode:revive", src)
-        SendChatMessage(src, {"You revive yourself!"}, {0, 255, 0})
+        exports.rsv_utils:SendChatMessage(src, {"You revive yourself!"}, {0, 255, 0})
         return
     elseif (#args > 1) then
-        SendChatMessage(src, {"Please insert only one argument."}, {255, 0, 0})
+        exports.rsv_utils:SendChatMessage(src, {"Please insert only one argument."}, {255, 0, 0})
         return
     end
     targetSrc = tonumber(args[1])
     if (targetSrc) then
         TriggerClientEvent("jph_gamemode:revive", targetSrc)
-        SendChatMessage(src, {"You revive", targetSrc, "!"}, {0, 255, 0})
+        exports.rsv_utils:SendChatMessage(src, {"You revive", targetSrc, "!"}, {0, 255, 0})
         return
     end
 end, true)
