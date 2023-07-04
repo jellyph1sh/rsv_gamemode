9e3d598038e66ccf92471b2070a59ccc694697c2
JellyPh1sh <sacha.sorgiati@gmail.com>
2023-07-04 17:06:28 +0200

    :wrench: | Update spawn location.

--- shared/config.lua
+++ shared/config.lua
@@ -1,4 +1,4 @@
 Config = {}
 
-Config.DefaultLocation = vector4(466.84, 197.72, 111.52, 291.71)
+Config.DefaultLocation = vector4(441.28, -987.80, 30.69, 358.22)
 Config.DefaultModel = "mp_m_freemode_01"
